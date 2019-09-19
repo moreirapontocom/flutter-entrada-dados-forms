7c8c0fffd377359f8dca4c9eb1b8db0c60c2b862
Lucas Moreira <moreirapontocom@gmail.com>
2019-09-19 19:33:41 -0300

    Using checkbox

--- lib/campoTexto.dart
+++ lib/campoTexto.dart
@@ -11,6 +11,8 @@ class _CampoTextoState extends State<CampoTexto> {
 
   String _nome = "Olá <<Desconhecido>>";
 
+  bool _checkbox = true;
+
   void _trocaNome() {
     setState(() {
       _nome = "Olá " + _textEditingController.text + "!";
@@ -26,40 +28,60 @@ class _CampoTextoState extends State<CampoTexto> {
         title: Text("Entrada de dados"),
         backgroundColor: Colors.blue,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(bottom: 15),
-              child: Text(_nome,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 15),
+                child: Text(_nome,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15
+                  ),
                 ),
               ),
-            ),
-            TextField(
-              keyboardType: TextInputType.text,
-              controller: _textEditingController,
-              decoration: InputDecoration(
-                labelText: "Escreva seu nome"
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child: RaisedButton(
-                color: Colors.blue,
-                child: Text(
-                  "Clique aqui",
-                  style: TextStyle(color: Colors.white),
+              TextField(
+                keyboardType: TextInputType.text,
+                controller: _textEditingController,
+                decoration: InputDecoration(
+                  labelText: "Escreva seu nome"
                 ),
-                onPressed: _trocaNome,
               ),
-            ),
-          ],
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: RaisedButton(
+                  color: Colors.blue,
+                  child: Text(
+                    "Clique aqui",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: _trocaNome,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Teste de Checkbox"),
+                    Checkbox(
+                      value: _checkbox,
+                      onChanged: (bool valor) {
+                        print("O valor do checkbox é: " + valor.toString());
+                        setState(() {
+                          _checkbox = valor;
+                        });
+                      },
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       )
     );
