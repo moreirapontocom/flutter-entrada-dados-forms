363fd360724f385234b3da8f3addb7a9add66705
Lucas Moreira <moreirapontocom@gmail.com>
2019-09-19 21:25:19 -0300

    Radio tiles

--- lib/myRadios.dart
+++ lib/myRadios.dart
@@ -9,6 +9,7 @@ class _MyRadiosState extends State<MyRadios> {
 
   // Este tipo é definido pelo tipo dos values dos radios que usam este groupValue
   String _groupValue;
+  // String _groupValue = "m"; // Pra já iniciar marcado
 
   @override
   Widget build(BuildContext context) {
@@ -19,9 +20,35 @@ class _MyRadiosState extends State<MyRadios> {
       ),
       body: Padding(
         padding: EdgeInsets.all(20),
-        child: Row(
+        child: Column( // Antes era Row e estava dando erro: "Another exception was thrown: RenderBox was not laid out". Acho que é pq estava ficando fora das margens.
+                       // O erro acima é pq o children possui um "...ListTile()" que é uma lista de alguma coisa. E listas são em colunas (um abaixo do outro).
           children: <Widget>[
 
+            RadioListTile(
+              title: Text("Masculino"),
+              value: "m", // Pode ser qualquer tipo: número, string...
+              groupValue: _groupValue,
+              activeColor: Colors.amber,
+              onChanged: (String valor) {
+                setState(() {
+                  _groupValue = valor;
+                });
+              },
+            ),
+
+            RadioListTile(
+              title: Text("Feminino"),
+              value: "f",
+              groupValue: _groupValue,
+              activeColor: Colors.amber,
+              onChanged: (String valor) {
+                setState(() {
+                  _groupValue = valor;
+                });
+              },
+            ),
+
+            /*
             Text("Masculino"),
             Radio(
               value: "m", // Pode ser qualquer tipo: número, string...
@@ -47,6 +74,7 @@ class _MyRadiosState extends State<MyRadios> {
                 });
               },
             )
+            */
 
           ],
         ),
