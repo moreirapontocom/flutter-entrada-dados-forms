5a2455bb790cac8e2cd799b8432eb171c309b6df
Lucas Moreira <moreirapontocom@gmail.com>
2019-09-19 23:12:23 -0300

    Navigating between screens

---
+++ lib/home.dart
@@ -0,0 +1,98 @@
+import 'package:entradadados/campoTexto.dart';
+import 'package:entradadados/myRadios.dart';
+import 'package:entradadados/mycheckbox.dart';
+import 'package:entradadados/myslider.dart';
+import 'package:entradadados/myswitch.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Curso Flutter"),
+        backgroundColor: Colors.indigo,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+
+            RaisedButton(
+              color: Colors.blue,
+              child: Text("Campos de texto", style: TextStyle(color: Colors.white)),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CampoTexto()
+                  )
+                );
+              },
+            ),
+
+            RaisedButton(
+              color: Colors.green,
+              child: Text("Checkboxes", style: TextStyle(color: Colors.white)),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MyCheckbox()
+                  )
+                );
+              },
+            ),
+
+            RaisedButton(
+              color: Colors.amber,
+              child: Text("Radios", style: TextStyle(color: Colors.white)),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MyRadios()
+                  )
+                );
+              },
+            ),
+
+            RaisedButton(
+              color: Colors.deepOrange,
+              child: Text("Sliders", style: TextStyle(color: Colors.white)),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MySlider()
+                  )
+                );
+              },
+            ),
+
+            RaisedButton(
+              color: Colors.cyan,
+              child: Text("Switches", style: TextStyle(color: Colors.white)),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MySwitches()
+                  )
+                );
+              },
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:entradadados/home.dart';
 // import 'package:entradadados/campoTexto.dart';
 // import 'package:entradadados/mycheckbox.dart';
 // import 'package:entradadados/myRadios.dart';
 // import 'package:entradadados/myswitch.dart';
-import 'package:entradadados/myslider.dart';
-import 'package:flutter/material.dart';
+// import 'package:entradadados/myslider.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "Entrada de dados",
+    home: HomePage()
     // home: CampoTexto()
     // home: MyCheckbox()
     // home: MyRadios()
     // home: MySwitches()
-    home: MySlider()
+    // home: MySlider()
   ));
 }
