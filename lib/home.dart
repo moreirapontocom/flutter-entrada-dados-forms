748e2c6bdc3e0d953405d77d54254e7df8b87744
Lucas Moreira <moreirapontocom@gmail.com>
2019-09-22 21:19:43 -0300

    SingleChildScrellView on home

--- lib/home.dart
+++ lib/home.dart
@@ -18,79 +18,81 @@ class _HomePageState extends State<HomePage> {
         title: Text("Curso Flutter"),
         backgroundColor: Colors.indigo,
       ),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
 
-            RaisedButton(
-              color: Colors.blue,
-              child: Text("Campos de texto", style: TextStyle(color: Colors.white)),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CampoTexto("Lucas Moreira")
-                  )
-                );
-              },
-            ),
+              RaisedButton(
+                color: Colors.blue,
+                child: Text("Campos de texto", style: TextStyle(color: Colors.white)),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CampoTexto("Lucas Moreira")
+                    )
+                  );
+                },
+              ),
 
-            RaisedButton(
-              color: Colors.green,
-              child: Text("Checkboxes", style: TextStyle(color: Colors.white)),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MyCheckbox()
-                  )
-                );
-              },
-            ),
+              RaisedButton(
+                color: Colors.green,
+                child: Text("Checkboxes", style: TextStyle(color: Colors.white)),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MyCheckbox()
+                    )
+                  );
+                },
+              ),
 
-            RaisedButton(
-              color: Colors.amber,
-              child: Text("Radios", style: TextStyle(color: Colors.white)),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MyRadios()
-                  )
-                );
-              },
-            ),
+              RaisedButton(
+                color: Colors.amber,
+                child: Text("Radios", style: TextStyle(color: Colors.white)),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MyRadios()
+                    )
+                  );
+                },
+              ),
 
-            RaisedButton(
-              color: Colors.deepOrange,
-              child: Text("Sliders", style: TextStyle(color: Colors.white)),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MySlider()
-                  )
-                );
-              },
-            ),
+              RaisedButton(
+                color: Colors.deepOrange,
+                child: Text("Sliders", style: TextStyle(color: Colors.white)),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MySlider()
+                    )
+                  );
+                },
+              ),
 
-            RaisedButton(
-              color: Colors.cyan,
-              child: Text("Switches", style: TextStyle(color: Colors.white)),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MySwitches()
-                  )
-                );
-              },
-            )
+              RaisedButton(
+                color: Colors.cyan,
+                child: Text("Switches", style: TextStyle(color: Colors.white)),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MySwitches()
+                    )
+                  );
+                },
+              )
 
-          ],
+            ],
+          ),
         ),
       ),
     );
